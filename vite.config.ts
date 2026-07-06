0edb237e54d7810ae60d25fbe25a39894ae3601e
Felipe Martin <FelipeTheFeh@gmail.com>
2026-07-06 01:27:36 -0300

    chore: commit automático do Fetch All — segunda-feira, 06/07/2026 01:27

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/For-Iris-Hub/spicy-game/',
+  base: './',
   plugins: [react()]
 })
